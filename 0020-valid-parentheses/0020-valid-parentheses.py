f1ab4e10fa11fd89cc444d708deeef7717f84eb1
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2026-01-09 13:55:25 -0500

    Time: 0 ms (100%), Space: 19.5 MB (12.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,8 +6,7 @@ class Solution:
         for i in s:
             if i in opens:
                 stack.append(i)
-            elif stack:
-                if ( i == ")" and stack[-1] == "(" ) or (i == "]" and stack[-1] == "[") or (i =="}" and stack[-1] == "{"):
+            elif stack and (( i == ")" and stack[-1] == "(" ) or (i == "]" and stack[-1] == "[") or (i =="}" and stack[-1] == "{")):
                     stack.pop()
             else:
                 stack.append(i)
