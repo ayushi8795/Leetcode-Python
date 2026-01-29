6584fdba38b63a59897f19271fa72f38455938ae
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2026-01-29 11:39:50 -0500

    Time: 4 ms (26.4%), Space: 19.2 MB (39.89%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        wordDic = {}
+        for char in paragraph:
+            if not char.isalnum():
+                paragraph = paragraph.replace(char, " ")
+        paragraph = paragraph.lower().split(" ")
+    
+        for i in paragraph:
+            if i not in banned and i.isalnum():
+                if i not in wordDic.keys():
+                    wordDic[i] = 1
+                else:
+                    wordDic[i] = wordDic[i]+1
+        print(wordDic)
+        maxVal = max(wordDic.values())
+        for key, value in wordDic.items():
+            if value == maxVal:
+                return key
