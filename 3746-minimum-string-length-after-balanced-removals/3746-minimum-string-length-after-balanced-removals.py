d7ac4bed5c49d4c3cc21d2d906838ee21ad4ff47
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2026-03-26 10:42:05 -0400

    Time: 74 ms (6.03%), Space: 20.8 MB (7.81%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minLengthAfterRemovals(self, s: str) -> int:
+        stack = []
+
+
+        for i in s:
+            if not stack:
+                stack.append(i)
+                print("if",stack)
+            else:
+                if i == "a" and stack[-1] =="b":
+                    stack.pop()
+                elif i == "b" and stack[-1] == "a":
+                    stack.pop()
+                else:
+                    stack.append(i)
+
+        return len(stack)
