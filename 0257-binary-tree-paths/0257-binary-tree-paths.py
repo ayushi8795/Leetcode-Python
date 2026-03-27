69b508f4cd3e8f3912bf5795fcdf2cebbfc95b84
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2026-03-26 21:00:15 -0400

    Time: 0 ms (100%), Space: 19.5 MB (46.92%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        
+
+        def dfs(root,path):
+            if  not root:
+                return 
+            path += str(root.val)
+
+            if not root.left and not root.right:
+                lis.append(path) # whole string formas here and appends to main list
+            else:
+                dfs(root.left, path + "->")
+                dfs(root.right, path + "->")
+        lis = []
+        dfs(root,"")
+        return lis
+
+
+
+                
